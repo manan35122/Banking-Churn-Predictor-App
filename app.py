f334f15e152b57796996e9c23c2228c7ac46107b
manan <mananarshad351@gmail.com>
2025-07-23 12:12:51 +0500

    Training Complete, Dashboard Creation for application and taking inputs(in-progress)

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import joblib
 
 st.title("🏦 Bank Customer Churn Prediction")
 
@@ -22,3 +25,22 @@ It contains information on bank customers who either left the bank or continue t
 - **EstimatedSalary**: The estimated salary of the customer  
 - **Exited**: Whether the customer has churned (1 = yes, 0 = no)
 """)
+st.header("Raw Dataset")
+df = pd.read_csv("Churn_Modelling.csv")
+st.dataframe(df)
+
+st.header("Cleaned Dataset Used for Training")
+df = pd.read_csv("cleaned_data.csv")
+df['HasCrCard'] = df["HasCrCard"].fillna(1)
+df['IsActiveMember'] = df["IsActiveMember"].fillna(1)
+df['Age'] = df["Age"].fillna(df["Age"].median())
+st.dataframe(df)
+
+st.header("Now lets predict for you ;)")
+cr_score = st.number_input("Credit Score",0,1000)
+age = st.number_input("Age:", 18,110)
+tenure = st.number_input("Tenure with Bank:", 0.0,20.0)
+balance = st.number_input('Balance:' , 0.0 , 3000000.0)
+NumProduct = st.number_input('Number of Products' , 0 , 10)
+CrCard = st.selectbox('Has Credit Card:',('No' , 'Yes'))
+CrCard = 1 if CrCard == 'Yes' else 0
